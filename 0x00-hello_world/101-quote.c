54363026cf35c58eda62bbc73e95c458036cf755
Hicham <hichamfakir300@gmail.com>
2023-03-16 19:57:23 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,10 +6,10 @@
  * folowed by a newline, to standart error
  * Return: Always 1
  */
-
 int main(void)
 
 {
-	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19,\n", 59);
+	write(2,
+	"and that piece of art is useful\" - Dora korpar, 2015-10-19,\n", 59);
 	return(1);
 }
