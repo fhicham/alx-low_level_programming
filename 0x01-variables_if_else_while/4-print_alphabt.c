ca2a6ca07c675a793a793c6fab13cc2c12fb48c4
Hicham <hichamfakir300@gmail.com>
2023-03-18 10:19:14 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,18 +6,19 @@
  */
 int main(void)
 {
-        int n = 97;
+	int n = 97;
 
-        while (n <= 122)
-        {
-                if (n == 101 || n == 113)
-		{
-			n++;
-			continue;
-		}
-		putchar(n);
-		n++;
+	while (n <= 122)
+	{
+	if (n == 101 || n == 113)
+	{
+	n++;
+	continue;
+	}
+	putchar(n);
+	n++;
 	}
 	putchar('\n');
 	return (0);
 }
+
