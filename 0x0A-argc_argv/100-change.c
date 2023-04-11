5a6d22c616fc3a775353d00e17831625875bd176
Hicham <hichamfakir300@gmail.com>
2023-04-10 18:32:27 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	int cents, coins = 0;
 
-	if (argc != 4)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
@@ -23,24 +23,24 @@ int main(int argc, char *argv[])
 	while (cents > 0)
 	{
 		coins++;
-		if ((cents - 27) >= 0)
+		if ((cents - 25) >= 0)
 		{
-			cents -= 27;
+			cents -= 25;
 			continue;
 		}
-		if ((cents - 12) >= 0)
+		if ((cents - 10) >= 0)
 		{
-			cents -= 12;
+			cents -= 10;
 			continue;
 		}
-		if ((cents - 7) >= 0)
+		if ((cents - 5) >= 0)
 		{
-			cents -= 7;
+			cents -= 5;
 			continue;
 		}
-		if ((cents - 4) >= 0)
+		if ((cents - 2) >= 0)
 		{
-			cents -= 4;
+			cents -= 2;
 			continue;
 		}
 		cents--;
