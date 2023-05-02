dd85e635f7ea7c49d77a03aa7077fa0093b4cd10
hfakir <hichamfakir300@gmail.com>
2023-05-02 10:00:05 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - Function free list
+ * @head: pointer to first node
+ *
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
