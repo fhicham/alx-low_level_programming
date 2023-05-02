d92b5430d80ac59ec1b531162e3384c9ce71d02e
hfakir <hichamfakir300@gmail.com>
2023-05-02 11:14:00 -0700

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_listint - The function of this total lists.
+ * @head: node header
+ *
+ * Return: soma
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next
+	}
+	return (sum);
+}
