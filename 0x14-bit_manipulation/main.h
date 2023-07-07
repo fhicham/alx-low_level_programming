7a9da9101d28a8caf1e8916713ad1a0a63c41acc
hfakir <hichamfakir300@gmail.com>
2023-07-06 17:22:31 -0700

    MAIN.H

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#define MAIN_H
+#ifndef MAIN_H
+
+#include <stdlib.h>
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+void _puts(char *str);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
