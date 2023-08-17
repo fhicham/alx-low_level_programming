a67c16f2e0767474aa275c68a09fb51947390448
hfakir <hichamfakir300@gmail.com>
2023-08-17 21:05:41 +0100

    print_dlistint

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all elements of a file
+ * print_dlistint - prints all the elements of a
  * dlistint_t list
- * 
- * @h: top of the list
- * Return: The number of points
+ *
+ * @h: head of the list
+ * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
@@ -27,3 +27,4 @@ size_t print_dlistint(const dlistint_t *h)
 	}
 
 	return (count);
+}
